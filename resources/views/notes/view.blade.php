5dea46243297196f8d46d4dee8a8ef31f9b00330
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-02-26 11:20:06 +0700

    feat: add heart count on notes

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -1,14 +1,14 @@
 <x-guest-layout>
     <div class="flex justify-between">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        <h2 class="mt-3 text-lg font-semibold leading-tight text-gray-800">
             {{ $note->title }}
         </h2>
     </div>
 
-    <p class="mt-4 mb-12">{{ $note->body }}</p>
+    <p class="mt-4 mb-12 text-sm text-justify">{{ $note->body }}</p>
 
-    <div class="flex items-center justify-end mt-12 space-x-2">
-        <h3 class="text-sm">Sent from {{ $user->name }}</h3>
+    <div class="flex items-center justify-end mt-12 space-x-1">
+        <h3 class="mr-2 text-sm">Sent from {{ $user->name }}</h3>
         <livewire:heartreact :note='$note' />
     </div>
 </x-guest-layout>
