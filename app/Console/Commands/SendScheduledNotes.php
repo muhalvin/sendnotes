fea57f0b668c27176efa299a359b40d143e10760
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-02-27 10:40:33 +0700

    feat: add scheduled email to send notes

--- app/Console/Commands/SendScheduledNotes.php
+++ app/Console/Commands/SendScheduledNotes.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\SendEmail;
+use App\Models\Note;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class SendScheduledNotes extends Command
@@ -18,13 +21,24 @@ class SendScheduledNotes extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Send scheduled notes';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $now = Carbon::now();
+
+        $notes = Note::where('is_published', true)
+            ->where('send_date', $now->toDateString())
+            ->get();
+        $noteCount = $notes->count();
+
+        $this->info("Sending {$noteCount} scheduled notes.");
+
+        foreach ($notes as $note) {
+            SendEmail::dispatch($note);
+        }
     }
 }
